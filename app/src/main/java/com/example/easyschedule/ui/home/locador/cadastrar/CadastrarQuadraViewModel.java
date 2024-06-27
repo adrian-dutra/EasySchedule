0ffce53b68fb9b0ff4e422d8f25b8da80ce80b2c
Ângelo Vinicius Hernandez Mazarin <angelotrab22@gmail.com>
2024-06-26 22:45:50 -0300

    [FIX] REMOVIDO PROJETO

--- app/src/main/java/com/example/easyschedule/ui/home/locador/cadastrar/CadastrarQuadraViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.easyschedule.ui.home.locador.cadastrar;
-
-import androidx.lifecycle.ViewModel;
-
-public class CadastrarQuadraViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/easyschedule/ui/home/locatario/Locar/LocarQuadraViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.easyschedule.ui.home.locatario.Locar;
-
-import androidx.lifecycle.ViewModel;
-
-public class LocarQuadraViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
