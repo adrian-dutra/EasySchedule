0526bf1eabdb200322c1f71b728ee76b6bccf529
Ângelo Vinicius Hernandez Mazarin <angelotrab22@gmail.com>
2024-06-29 02:07:24 -0300

    [FEAT] Locacoes funciona

--- EasyScheduleV20/app/src/main/java/com/example/easyschedulev20/view/locador/ui/locacao/LocacaoQuadraLocadorFragment.java
+++ EasyScheduleV20/app/src/main/java/com/example/easyschedulev20/view/locador/ui/locacao/LocacaoQuadraLocadorFragment.java
@@ -4,27 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.easyschedulev20.R;
 import com.example.easyschedulev20.databinding.FragmentLocacaoQuadraLocadorBinding;
+import com.example.easyschedulev20.model.LocacaoQuadra;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LocacaoQuadraLocadorFragment extends Fragment {
 
     private FragmentLocacaoQuadraLocadorBinding binding;
+    private ListView listView_quadras_locador;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         LocacaoQuadraLocadorViewModel locacaoQuadraLocadorViewModel =
-                new ViewModelProvider(requireActivity()).get(LocacaoQuadraLocadorViewModel.class);
+                new ViewModelProvider(this).get(LocacaoQuadraLocadorViewModel.class);
 
         binding = FragmentLocacaoQuadraLocadorBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
+        listView_quadras_locador = root.findViewById(R.id.listView_quadras_locador);
+        List<LocacaoQuadra> locacoes = locacaoQuadraLocadorViewModel.listarLocacoes(requireActivity().getApplication());
+        List<String> nomesQuadras = new ArrayList<>();
+        for (LocacaoQuadra locacao : locacoes) {
+            nomesQuadras.add(locacao.getHoraInicio());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, nomesQuadras);
+        listView_quadras_locador.setAdapter(adapter);
 
         return root;
     }
--- EasyScheduleV20/app/src/main/java/com/example/easyschedulev20/view/locador/ui/locacao/LocacaoQuadraLocadorViewModel.java
+++ EasyScheduleV20/app/src/main/java/com/example/easyschedulev20/view/locador/ui/locacao/LocacaoQuadraLocadorViewModel.java
@@ -1,14 +1,27 @@
 package com.example.easyschedulev20.view.locador.ui.locacao;
 
+import android.app.Application;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.easyschedulev20.model.LocacaoQuadra;
+import com.example.easyschedulev20.model.Repository.LocacaoQuadraRepository;
+
+import java.util.List;
+
 public class LocacaoQuadraLocadorViewModel extends ViewModel {
 
+    private LocacaoQuadraRepository locacaoQuadraRepository;
 
     public LocacaoQuadraLocadorViewModel() {
 
+    }
+
+    public List<LocacaoQuadra> listarLocacoes(Application context){
+        locacaoQuadraRepository = new LocacaoQuadraRepository(context);
+        return locacaoQuadraRepository.listarTodasAsLocacoes();
     }
 
 }
